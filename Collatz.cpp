aaef270ff0dae87c1e41fbeb4578fa815a53d72a
B4rtekk1 <113917881+B4rtekk1@users.noreply.github.com>
2024-09-22 19:30:49 +0200

    Add files via upload

---
+++ Collatz.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, n1, n2, l, lmax;
+    lmax = 0;
+    cin >> n1 >> n2;
+
+    for (int i = n1; i <= n2; i++)
+    {
+        l = 0;
+        n = i;
+        while (n > 1)
+        {
+            if (n % 2 == 0)
+            {
+                n = n / 2;
+            }
+            else
+            {
+                n = 3 * n + 1;
+            }
+            l++;
+            //cout << n << endl;
+        }
+        if (l > lmax)
+        {
+            lmax = l;
+        }
+    }
+
+    cout << n1 <<" " << n2 <<" " << lmax + 1;
+}
+
